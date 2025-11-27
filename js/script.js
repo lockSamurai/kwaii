3d0553a6fc0359aff9f4e2eaf7d1f4ce065dd1f6
yDreadsamurai <arturgomes486@gmail.com>
2025-11-27 20:31:50 -0300

    feat: mudança de tema e conteudo

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const title = document.querySelector('.presentation-text h1').textContent;
+});
--- teste.js
+++
@@ -1,4 +0,0 @@
-const img= document.getElementById("foto");
-
-img.addEventListener("mouseenter", () => img.classList.add("is-expanded"))
-img.addEventListener("mouseleave", () => img.classList.remove("is-expanded"))
